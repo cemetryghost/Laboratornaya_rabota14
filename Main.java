0677ca6541762a98396cbb028576a7aee7cf55b6
cemetryghost <96484941+cemetryghost@users.noreply.github.com>
2022-05-05 23:57:37 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.io.*;
+import java.util.Scanner;
+
+class Function implements Serializable {
+    double x, y;
+
+    double getY() {
+        y = this.x - Math.sin(this.x);
+        return y;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String txt;
+        Scanner in = new Scanner(System.in);
+        Function func = new Function();
+        System.out.print("Enter value: ");
+        int i = 1;
+        while (true) {
+            txt = in.nextLine();
+            try {
+                double x = Double.parseDouble(txt);
+                func.x = x;
+                func.getY();
+                if(i == 1){
+                    System.out.println("You can enter one of the following commands: save, upload, check, stop");
+                    i++;
+                }
+            } catch (Exception IOe) {
+                if (txt.equalsIgnoreCase("save")) {
+                    try (ObjectOutputStream wr = new ObjectOutputStream(new FileOutputStream("object.txt"))) {
+                        wr.writeObject(func);
+                        System.out.println("Data saved to file");
+                    } catch (IOException ex) {
+                        ex.getMessage();
+                    }
+                } else if (txt.equalsIgnoreCase("upload")) {
+                    try (ObjectInputStream rd = new ObjectInputStream(new FileInputStream("object.txt"))) {
+                        func = (Function) rd.readObject();
+                        System.out.println("Upload successful");
+                    } catch (Exception ex) {
+                        ex.getMessage();
+                    }
+                } else if (txt.equalsIgnoreCase("check")) {
+                    System.out.printf("x: %s\ny: %s\n",func.x, func.y);
+                } else if (txt.equalsIgnoreCase("stop")){
+                    break;
+                }
+                else {
+                    System.out.println("Such a command does not exist");
+                }
+                System.out.println("You can enter one of the following commands: save, upload, check, stop");
+            }
+        }
+    }
+}
